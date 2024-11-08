47d1881b392d6a8dedd4bb685f4813e517538ec8
JaNiah Harris <136260706+janiahharris205@users.noreply.github.com>
2024-11-08 16:23:03 -0600

    Update key.js

--- src/key.js
+++ src/key.js
@@ -1,4 +1,4 @@
 module.exports = {
-    PLAID_CLIENT_ID: "66e0bb4c03669c00194d4eb",
-    PLAID_SECRET: "31df21d6cc1632bfd3ca573cad75d5"
+    PLAID_CLIENT_ID: "REDACTED",
+    PLAID_SECRET: "REDACTED"
 };
